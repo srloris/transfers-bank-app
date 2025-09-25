03ce7b15b81aad82957d04e3ed3e552848e7e5ec
Lorenzo Nunez <124052885+srloris@users.noreply.github.com>
2025-09-25 01:09:36 -0300

    feat/controllers: Adiciona o controller e advance handler

---
+++ backend/src/main/java/com/acme/transfers/backend/controllers/TransferController.java
@@ -0,0 +1,40 @@
+package com.acme.transfers.backend.controllers;
+
+import com.acme.transfers.backend.dto.TransferRequestDTO;
+import com.acme.transfers.backend.dto.TransferResponseDTO;
+import com.acme.transfers.backend.services.TransferService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "api/transfer")
+@AllArgsConstructor
+public class TransferController {
+
+    private final TransferService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<TransferResponseDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<TransferResponseDTO>> findAll(Pageable pageable) {
+        return ResponseEntity.ok(service.findAll(pageable));
+    }
+
+    @PostMapping
+    public ResponseEntity<TransferResponseDTO> create(@Valid @RequestBody TransferRequestDTO requestDTO) {
+        TransferResponseDTO responseDTO = service.create(requestDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(responseDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(responseDTO);
+    }
+}
---
+++ backend/src/main/java/com/acme/transfers/backend/controllers/handlers/ControllerAdvanceHandler.java
@@ -0,0 +1,43 @@
+package com.acme.transfers.backend.controllers.handlers;
+
+import com.acme.transfers.backend.dto.CustomError;
+import com.acme.transfers.backend.dto.ValidationError;
+import com.acme.transfers.backend.services.Exceptions.InvalidTransferDateException;
+import com.acme.transfers.backend.services.Exceptions.TransferNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerAdvanceHandler {
+
+    @ExceptionHandler(TransferNotFoundException.class)
+    public ResponseEntity<CustomError> transferNotFound(TransferNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(InvalidTransferDateException.class)
+    public ResponseEntity<CustomError> invalidTransferDate(InvalidTransferDateException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity <CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(),"Invalid data", request.getRequestURI());
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+        return ResponseEntity.status(status).body(err);
+    }
+}
