307b45f07c65019f2e29d5d6f17466573fdb7851
Lorenzo Nunez <124052885+srloris@users.noreply.github.com>
2025-09-25 03:10:17 -0300

    feat/App: Adiciono tela inicial com chamadas aos componentes

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container mt-5">
+    <h3 class="mb-4">Transfer System</h3>
+    <TransferForm @refresh="refreshList" />
+    <TransferList ref="list" />
+  </div>
+</template>
+
+<script>
+import TransferForm from "./components/TransferForm.vue";
+import TransferList from "./components/TransferList.vue";
+
+export default {
+  components: { TransferForm, TransferList },
+  methods: {
+    refreshList() {
+      this.$refs.list.load();
+    },
+  },
+};
+</script>
