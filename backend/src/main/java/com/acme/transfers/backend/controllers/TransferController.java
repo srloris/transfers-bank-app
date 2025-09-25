322340581cf8cc064dacf16d5f59f350a3ce6f26
Lorenzo Nunez <124052885+srloris@users.noreply.github.com>
2025-09-25 03:59:15 -0300

    fix/controller: Ajuste na rota

--- backend/src/main/java/com/acme/transfers/backend/controllers/TransferController.java
+++ backend/src/main/java/com/acme/transfers/backend/controllers/TransferController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 import java.net.URI;
 
 @RestController
-@RequestMapping(value = "api/transfer")
+@RequestMapping(value = "/transfer")
 @AllArgsConstructor
 public class TransferController {
 
